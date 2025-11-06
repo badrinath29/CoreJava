0c0f454a4c755ccc3f3911f486ade193b9eea0e3
badrinath-2902 <bmatam@techtiera.net>
2025-11-06 15:06:54 +0530

    initial commit

---
+++ src/Class/Dog.java
@@ -0,0 +1,13 @@
+package Class;
+
+public class Dog {
+	public String breed="German Shepherd";
+	public  int Height=4;
+	public  int weight =30;
+
+    public static void main(String[] args) 
+    {
+	Dog obj=new Dog();
+    System.out.println(obj.Height);
+    }
+}
